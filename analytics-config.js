c9b3d5f8fd5030436775f0513345e929bec2fdb3
hiteshnagpal955 <hiteshnagpal955@gmail.com>
2026-09-20 23:55:30 -0500

    Add files via upload

---
+++ analytics-config.js
@@ -0,0 +1,4 @@
+// Cloudflare Web Analytics site token.
+// This is NOT an API secret. It is designed to appear in public website code.
+// Analytics stays OFF while the value is blank.
+window.PORTFOLIO_ANALYTICS_TOKEN = "";
